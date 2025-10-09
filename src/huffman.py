4920b677fc9c367bb2b284e36a8beb7dc11b6e1f
eduardodpms <eduardopms19@hotmail.com>
2025-10-08 23:18:56 -0300

    feat: implementar prefix codes
    Co-authored-by: pedroeverton217 <pedro.everton217@gmail.com>

--- src/huffman.py
+++ src/huffman.py
@@ -1,4 +1,4 @@
-from src.heapq_alt import heapify, heappush, heappop
+from heapq_alt import heapify, heappush, heappop
 
 
 def char_count(text):
@@ -10,13 +10,38 @@ def char_count(text):
 def prefix_tree(count):
     heapify(count)
     while(len(count)>1):
-        value_0 = heappop(count)
-        value_1 = heappop(count)
-        x = value_0[0] + value_1[0]
-        y = [value_0[1], value_1[1]]
-        value = (x, y)
+        x = heappop(count)
+        y = heappop(count)
+        value = (x[0] + y[0], [x[1], y[1]])
         heappush(count, value)
-    return count[0][1]
+    return count[0][1] # Esquema de vetores representando a árvore de prefixos
 
 def prefix_codes(tree):
-    pass
+    codes, prefix = {}, ''
+    tree = [tree]
+    current_branch = tree
+
+    while tree != [None]:
+        #print(f'>>> {codes} // {prefix} // {tree}')
+
+        if(current_branch[0] != None):
+            n = 0
+        elif(current_branch[-1] != None):
+            n = 1
+        else:
+            current_branch = tree
+            for c in prefix[:-1]:
+                current_branch = current_branch[int(c)]
+            current_branch[int(prefix[-1])] = None
+            prefix = prefix[:-1]
+            continue
+
+        if(type(current_branch[n]) == list):
+            prefix += f'{n}'
+            current_branch = current_branch[n]
+        elif(type(current_branch[n]) == str):
+            binary = prefix + str(n)
+            codes[current_branch[n]] = binary[1:]
+            current_branch[n] = None
+    return codes
+
